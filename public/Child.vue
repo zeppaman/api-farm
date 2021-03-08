a48004c426064bb5757530a3c8351deb7275e885
Daniele Fontani <>
2021-03-08 17:16:43 +0100

    g

---
+++ public/Child.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+    ss
+</div>
+</template>
+
+
+
+<script >
+
+
+  window.parent= window.httpVueLoader.register(Vue,"Parent.vue").apply();
+
+// console.log(parent);
+//    var Component = parent.extend({
+//        data() {
+//                 return {
+//                     valore:"child"
+//                 }
+//           }
+//    });
+
+//    window.parent=parent;
+
+// //    window.parent.call().then(x=>{
+// //       // console.log(x);
+// //         var Component = Vue.extend({
+// //             mixins: [parent]
+// //         });
+
+// //         console.log(Component);
+// //         module.exports = new Component();
+// //    });
+
+//     var component= httpVueLoader('Parent.vue').load().resolve();
+//      window.component=component;
+
+   
+
+
+
+  module.exports = new Component();
+</script>
---
+++ public/Parent.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    {{valore}}
+</div>
+</template>
+
+
+
+<script>
+
+    httpVueLoader.component(Vue,"Parent.vue")
+    module.exports= {
+          data() {
+                return {
+                    valore:"parent"
+                }
+          }
+    }
+</script>
